9e59b10069986a0193d1dead83334a914c84fb5e
胡斌 <hubin@weifengqi18.com>
2022-12-21 21:03:02 +0800

    feat: data type

---
+++ 02.数据类型详细.dart
@@ -0,0 +1,68 @@
+void main() {
+  // String
+
+  // 1. 模板字符串
+  // var s = "ice";
+  // var num = 123;
+  // print('my name is $s');
+  // // 方法要用括号包裹
+  // print('num is ${num.toString()}');
+  // 2. 拼接字符串即便不在同一行
+  // var s1 = 'String '
+  // 'concatenation'
+  // " works even over line breaks.";
+  // print(s1 == 'String concatenation works even over line breaks.');
+
+  // Bool 布尔类型 (判断与js截然不同)
+
+  // 1.必须显示调用
+  // var fullName = '';
+  // if (fullName.isEmpty) {
+  //   print('is true');
+  // }
+  // var hitPoints = 0;
+  // if (hitPoints == 0) print(1);
+
+  // List 数组集合类型
+
+  // 1. 定义的方式
+  // var list1 = [1, 2, 3];
+  // var list3 = null;
+  // List list2 = [1, 2, 3];
+  // print(list1 == list2);
+
+  // 2. const 不能修改内容 js中不能修改内存地址
+  // var list = [1, 2, 3];
+  // list[1] = 3;
+  // print(list);
+
+  // 3. 支持扩展操作符 空感知扩展操作符
+  // var list2 = [...list1, 4, 6, ...?list3];
+  // print(list2);
+
+  // 4. 集合中的 if 和 集合中的 for
+  // var list2 = [
+  //   ...list1,
+  //   if (true) 99,
+  //   for (var i in [4, 5, 6]) i
+  // ];
+  // print(list2);
+
+  // Sets 无序集合
+  // 1. 定义方式
+  // var friends = {'ice', 'panda', 'quezz'};
+  // Set friends = {'ice', 'panda', 'quezz'};
+
+  // 2. 创建map,而非sets
+  // const maps = {};
+  // print(maps is Set);
+  // print(maps is Map);
+
+  // 3. 增加无需集合
+  // friends.add('1');
+  // friends.addAll({'2', '3'});
+  // print(friends.length);
+
+
+  // Maps 对象映射
+}
